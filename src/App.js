a00ab9efc89f52fe28155071950c1d826301d3dc
leeseungwon <tmddnjs2385@naver.com>
2020-07-27 20:44:46 +0900

    add Router and Navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
+import { HashRouter, Route, Router } from "react-router-dom";
+import About from "./routes/About.js";
+import Home from "./routes/Home.js";
+import Navigation from "./Components/Navigation";
 
 function App() {
-
   return (
-    <HashRouter>
-      <Route path="/about" component={About} />
-    </HashRouter>
-  );
 
+    // brower router 보다 hash router가 git-hub에 page 연동하는 것이 쉽다. 
+    // NAvaigation은 항상 Router 안에 있어야 한다. 그래야 작동한다.  밖에 있으면 작동하지 않는다. 
+
+    <HashRouter>
+
+      <Navigation />
+
+      <Route path="/" exact={true} component={Home} />
+
+      <Route path="/about" component={About} />
+
+    </HashRouter>
+
+  );
 };
 
 export default App;
