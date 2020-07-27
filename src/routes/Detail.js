21b12b6f67782efe1ce03091658dcef40711ad23
leeseungwon <tmddnjs2385@naver.com>
2020-07-27 22:12:03 +0900

    New Component Detail

---
+++ src/routes/Detail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Detail(props) {
+
+    console.log(props)
+
+    return (
+        <div>
+            summary
+        </div>
+    );
+}
+
+export default Detail;
