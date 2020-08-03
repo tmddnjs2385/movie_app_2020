2a18cccc4d59f0767807affc1ee5996a46a3d688
leeseungwon <tmddnjs2385@naver.com>
2020-08-03 15:42:48 +0900

    add className

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import "./About.css";
 
 function About(props) {
+
     console.log(props);
 
     return (
-        <div>
+        <div className="about">
             <span>About this page: I built it because I love movies. </span>
         </div>
     );
