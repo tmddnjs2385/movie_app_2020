178788d4759001cc72ee8dc9a7cd0e97469c5c76
leeseungwon <tmddnjs2385@naver.com>
2020-08-03 15:43:36 +0900

    add componentDidMount

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import "./Detail.css";
+class Detail extends Component {
 
-function Detail(props) {
 
-    console.log(props)
+    componentDidMount() {
+        const { location, history } = this.props;
+        console.log(location);
 
-    return (
-        <div>
-            summary
-        </div>
-    );
-}
+        if (location.state === undefined) {
+            history.push("/");
+        }
+
+
+    }
+
+    render() {
+        const { location } = this.props;
+        console.log(location);
+        if (location.state) {
+            return (
+                <div className="detail" >
+                    {location.state.summary}
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+};
 
 export default Detail;
